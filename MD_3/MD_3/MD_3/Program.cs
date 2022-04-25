3a0dda0b72abb12564f82cb485f3a5f5f6c6bcad
DainaSvalbe <svalbedaina@gmail.com>
2022-04-26 01:17:22 +0300

    Telefons

--- MD_3/MD_3/MD_3/Program.cs
+++ MD_3/MD_3/MD_3/Program.cs
@@ -1,3 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using MD_3;
+
 Console.WriteLine("Hello, World!");
 //----------------------------
+
+Telefons person = new Telefons();
+
+person.Marka = "Samsung";
+person.Modelis = "S20";
+person.IzmersX = 200;
+person.IzmersY = 100;
+person.IzmersZ = 30;
+
+person.Call();
+person.Sms();
---
+++ MD_3/MD_3/MD_3/Telefons.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MD_3
+{
+    public class Telefons
+    {
+
+        public string Marka { get; set; }
+
+        public string Modelis { get; set; }
+
+        public int IzmersX { get; set; }
+
+        public int IzmersY { get; set; }
+
+        public int IzmersZ { get; set; }
+
+
+        public void Call()
+        {
+            Console.WriteLine("Calling");
+        }
+
+        public void Sms()
+        {
+            Console.WriteLine("Send SMS");
+        }
+    }
+}
